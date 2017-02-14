laurent90/dynare

commit fe09ffc79fbd6eba08a3e2765b69622908ea391e

Account for loglinear option when reading in M_.endo_histval

matlab/dyn_forecast.m
@@ -73,9 +73,17 @@ switch task
         horizon = 5;
     end
     if isempty(M.endo_histval)
-        y0 = repmat(oo.dr.ys,1,maximum_lag);
+        if options.loglinear && ~options.logged_steady_state
+            y0 = repmat(log(oo.dr.ys),1,maximum_lag);
+        else
+            y0 = repmat(oo.dr.ys,1,maximum_lag);
+        end
     else
-        y0 = M.endo_histval;
+        if options.loglinear
+            y0 = log_variable(1:M_.endo_nbr,M_.endo_histval,M_);
+        else
+            y0 = M.endo_histval;
+        end
     end
   case 'smoother'
     horizon = options.forecast;

matlab/disp_moments.m
@@ -132,7 +132,11 @@ if ~options_.nodecomposition
         if isempty(M_.endo_histval)
             y0 = oo_.dr.ys;
         else
-            y0 = M_.endo_histval;
+            if options_.loglinear
+                y0 = log_variable(1:M_.endo_nbr,M_.endo_histval,M_);
+            else
+                y0 = M_.endo_histval;
+            end
         end
         %back out shock matrix used for generating y
         i_exo_var = setdiff([1:M_.exo_nbr],find(diag(M_.Sigma_e) == 0)); % find shocks with 0 variance

matlab/imcforecast.m
@@ -162,7 +162,20 @@ else
     end
 end
 
+if options_.logged_steady_state %if steady state was previously logged, undo this
+    oo_.dr.ys=exp(oo_.dr.ys);
+    oo_.steady_state=exp(oo_.steady_state);
+    options_.logged_steady_state=0;
+end
+
 [T,R,ys,info,M_,options_,oo_] = dynare_resolve(M_,options_,oo_);
+
+if options_.loglinear && isfield(oo_.dr,'ys') && options_.logged_steady_state==0 %log steady state
+    oo_.dr.ys=log_variable(1:M_.endo_nbr,oo_.dr.ys,M_);
+    ys=oo_.dr.ys;
+    oo_.steady_state=log_variable(1:M_.endo_nbr,oo_.steady_state,M_);
+    options_.logged_steady_state=1; %set option for use in stoch_simul
+end
 
 if ~isdiagonal(M_.Sigma_e)
     warning(sprintf('The innovations are correlated (the covariance matrix has non zero off diagonal elements), the results of the conditional forecasts will\ndepend on the ordering of the innovations (as declared after varexo) because a Cholesky decomposition is used to factorize the covariance matrix.\n\n=> It is preferable to declare the correlations in the model block (explicitly imposing the identification restrictions), unless you are satisfied\nwith the implicit identification restrictions implied by the Cholesky decomposition.'))
@@ -175,7 +188,14 @@ if ~estimated_model
     if isempty(M_.endo_histval)
         y0 = ys;
     else
-        y0 = M_.endo_histval;
+        if options_.loglinear
+            %make sure that only states are updated (controls have value of 0 in vector)
+            y0=zeros(size(ys));
+            y0_logged = log_variable(1:M_.endo_nbr,M_.endo_histval,M_);
+            y0(M_.endo_histval~=0)=y0_logged(M_.endo_histval~=0);
+        else
+            y0 = M_.endo_histval;
+        end
     end
     InitState(:,1) = y0(oo_.dr.order_var)-ys(oo_.dr.order_var,:); %initial state in deviations from steady state
     trend = repmat(ys(oo_.dr.order_var,:),1,options_cond_fcst.periods+1); %trend needs to contain correct steady state

matlab/simulated_moment_uncertainty.m
@@ -12,7 +12,7 @@ function [cmm, mm] = simulated_moment_uncertainty(indx, periods, replic,options_
 %   - cmm:      [n_moments by n_moments] covariance matrix of simulated moments
 %   - mm:       [n_moments by replic] matrix of moments
 %  
-% Copyright (C) 2009-2016 Dynare Team
+% Copyright (C) 2009-2017 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -80,9 +80,17 @@ end
 
 %set starting point of simulations
 if isempty(M_.endo_histval)
-    y0 = oo_.dr.ys;
+    if options_.loglinear
+        y0 = log(oo_.dr.ys);
+    else
+        y0 = oo_.dr.ys;
+    end
 else
-    y0 = M_.endo_histval;
+    if options_.loglinear
+        y0 = log_variable(1:M_.endo_nbr,M_.endo_histval,M_);
+    else
+        y0 = M_.endo_histval;
+    end
 end
 
 

matlab/stoch_simul.m
@@ -150,7 +150,11 @@ if options_.periods > 0 && ~PI_PCL_solver
     if isempty(M_.endo_histval)
         y0 = oo_.dr.ys;
     else
-        y0 = M_.endo_histval;
+        if options_.loglinear
+            y0 = log_variable(1:M_.endo_nbr,M_.endo_histval,M_);
+        else
+            y0 = M_.endo_histval;
+        end
     end
     [ys, oo_] = simult(y0,oo_.dr,M_,options_,oo_);
     oo_.endo_simul = ys;

commit 03e836caf1f33bae563a43543096e989e3423bb5

Fix copy and paste mistakes introduced in fe09ffc79fbd6eba08a3e2765b69622908ea391e

matlab/dsge_likelihood.m
@@ -440,7 +440,7 @@ switch DynareOptions.lik_init
             end
         end
         a_inital     = zeros(mmm,1);
-        a=set_Kalman_starting_values(a,Model,DynareResults,DynareOptions,BayesInfo);
+        a_inital=set_Kalman_starting_values(a_inital,Model,DynareResults,DynareOptions,BayesInfo);
         [dLIK,dlik,a,Pstar] = univariate_kalman_filter_d(DatasetInfo.missing.aindex,...
                                                         DatasetInfo.missing.number_of_observations,...
                                                         DatasetInfo.missing.no_more_missing_observations, ...

matlab/dyn_forecast.m
@@ -80,7 +80,7 @@ switch task
         end
     else
         if options.loglinear
-            y0 = log_variable(1:M_.endo_nbr,M_.endo_histval,M_);
+            y0 = log_variable(1:M.endo_nbr,M.endo_histval,M);
         else
             y0 = M.endo_histval;
         end